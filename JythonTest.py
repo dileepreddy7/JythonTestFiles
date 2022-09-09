229ebcf77dd83fb7825a952c8901033cff22df94
dileepreddy <55627599+dileepreddy7@users.noreply.github.com>
2022-09-09 18:42:33 +0530

    Jython files added.

---
+++ JythonTest.py
@@ -0,0 +1,6 @@
+print("hello world")
+print(FD_PROJECT_NAME)
+#print(JYTHONINPUTSTRING)
+# adsf
+# aatwewtwer
+# afdasdf
---
+++ JythonTestPartial.py
@@ -0,0 +1,8 @@
+print("hello world")
+print(FD_PROJECT_NAME)
+print(FILE_PATH)
+#print(JYTHONINPUTSTRING)
+#asdf
+#afasdfasdf
+#asdf
+#afe
